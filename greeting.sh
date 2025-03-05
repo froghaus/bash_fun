657063fb97eaffaa25026116d23cc2c63687fd06
Nick Adams <108083920+froghaus@users.noreply.github.com>
2025-03-05 16:34:23 -0500

    Update greeting.sh

--- greeting.sh
+++ greeting.sh
@@ -1,5 +1,5 @@
 # possible greetings
-GREETINGS=("Hello!" "Prithee tell, what is poppin?" "Hail, and well met" "Buongiorno")
+GREETINGS=("Hello!" "Prithee tell, what is poppin?" "Hail, and well met" "Buongiorno" "Bonjour" "Guten tag" "Hallo" "Privet" "Buenos dias" "Ni hao" "Konnichiwa")
 
 # randomly select a greeting
 RANDOM_INDEX=$(( RANDOM % ${#GREETINGS[@]} ))
