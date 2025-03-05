83b8dd407dea68c00ec776036dbbf8130e4c7a07
Nick Adams <108083920+froghaus@users.noreply.github.com>
2025-03-05 16:20:43 -0500

    Update greeting_setup.sh

--- greeting_setup.sh
+++ greeting_setup.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 
 apt install figlet
 
-echo "alias greeting='source <(curl -s https://raw.githubusercontent.com/froghaus/bash_fun/refs/heads/main/greeting.sh)'" >> ~/.bashrc
+echo "alias hello_computer='source <(curl -s https://raw.githubusercontent.com/froghaus/bash_fun/refs/heads/main/greeting.sh)'" >> ~/.bashrc
 source ~/.bashrc
 
 greeting
